a787ad4a429566d3300a874486b5a2b4c3e14cb9
Matthew Williamson <matthew.williamson@aie.edu.au>
2019-06-05 11:49:36 -0500

    merge master and lodis

---
+++ Assets/Scripts/Lodis/GateBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+namespace Lodis
+{
+
+
+
+    public class GateBehaviour : MonoBehaviour
+    {
+        [Cinemachine.TagField, SerializeField]
+        string compareTag;
+        [SerializeField]
+        private CinemachineVirtualCamera currentCam;
+        [SerializeField]
+        private CinemachineVirtualCamera transitionCam;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(compareTag))
+            {
+                currentCam.enabled = !currentCam.enabled;
+                transitionCam.enabled = !transitionCam.enabled;
+            }
+        }
+
+    }
+}
