83bca6ac5b5cb0c724fd8ca71b67dbf5f9324d84
Lodis Perkins <s188043@students.aie.edu>
2019-06-05 09:26:55 -0500

    Created Gate trigger and Gate Behaviour script

--- Assets/Scripts/Lodis/AgentBehaviour.cs
+++ Assets/Scripts/Lodis/AgentBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class AgentBehaviour : MonoBehaviour {
     private Vector3 playerInput;
     [SerializeField]
---
+++ Assets/Scripts/Lodis/GateBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class GateBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private CinemachineVirtualCamera currentCam;
+    [SerializeField]
+    private CinemachineVirtualCamera transitionCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentCam.enabled = !currentCam.enabled;
+            transitionCam.enabled = !transitionCam.enabled;
+        }
+    }
+
+}
