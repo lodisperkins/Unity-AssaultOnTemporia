b8bb4ae1b75ff99ce571c3f948af5efe5cb8418b
Jordon Cotton <s188044@students.aie.edu>
2019-06-07 12:54:20 -0500

    create the NPC Behaviour script

---
+++ Assets/Scripts/Cotton/NPCBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Cotton
+{
+    public class NPCBehaviour : MonoBehaviour
+    {
+        private int mynumber;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
