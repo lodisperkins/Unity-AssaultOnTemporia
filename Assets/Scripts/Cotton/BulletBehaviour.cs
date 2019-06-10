46ea0c6416c92050673e4b50c7d850b3569e5cbb
Jordon Cotton <s188044@students.aie.edu>
2019-06-10 18:01:59 -0500

    file for bullet and NPC behaviour

--- Assets/Scripts/Cotton/BulletBehaviour.cs
+++ Assets/Scripts/Cotton/BulletBehaviour.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBehaviour : MonoBehaviour {
+public class BulletBehaviour : MonoBehaviour
+{
+    public GameObject Bullet_Emitter;
+    public GameObject Bullet;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public float Bullet_Forward_Force;
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetKeyDown("space"))
+        {
+            GameObject Temporary_Bullet_handler;
+            Temporary_Bullet_handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            Bullet_Emitter.transform.Rotate(Vector3.left * 90);
+
+            Rigidbody Temporary_rigidbody;
+            Temporary_rigidbody = Temporary_Bullet_handler.GetComponent<Rigidbody>();
+
+            Temporary_rigidbody.AddForce(transform.forward * Bullet_Forward_Force);
+            Destroy(Temporary_Bullet_handler, 10.0f);
+        }
 	}
 }
--- Assets/Scripts/Cotton/NPCBehaviour.cs
+++ Assets/Scripts/Cotton/NPCBehaviour.cs
@@ -1,21 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
 
 namespace Cotton
 {
     public class NPCBehaviour : MonoBehaviour
     {
-        private int mynumber;
+        public float moveSpeed = 3f;
+        public float rotSpeed = 100f;
 
-        // Use this for initialization
-        void Start()
-        {
+        private bool isWandering = false;
+        private bool isRotatingLeft = false;
+        private bool isRotatingRight = false;
+        private bool isWalking = false;
 
-        }
-
-        // Update is called once per frame
         void Update()
         {
+            if (isWandering == false)
+            {
+                StartCoroutine(Wander());
+            }
+            if (isRotatingRight == true)
+            {
+                transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+            }
+            if (isRotatingLeft == true)
+            {
+                transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+            }
+            if (isWalking == true)
+            {
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+        }
 
+        IEnumerator Wander()
+        {
+            int rotTime = Random.Range(1, 3);
+            int rotateWait = Random.Range(1, 4);
+            int rotateLorR = Random.Range(1, 2);
+            int walkWait = Random.Range(1, 4);
+            int walkTime = Random.Range(1, 5);
+
+            isWandering = true;
+            yield return new WaitForSeconds(walkWait);
+            isWalking = true;
+            yield return new WaitForSeconds(walkTime);
+            isWalking = false;
+            yield return new WaitForSeconds(rotateWait);
+
+            if(rotateLorR == 1)
+            {
+                isRotatingRight = true;
+                yield return new WaitForSeconds(rotTime);
+                isRotatingRight = false;
+            }
+
+            if (rotateLorR == 2)
+            {
+                isRotatingLeft = true;
+                yield return new WaitForSeconds(rotTime);
+                isRotatingLeft = false;
+            }
+            isWandering = false;
         }
     }
 }
