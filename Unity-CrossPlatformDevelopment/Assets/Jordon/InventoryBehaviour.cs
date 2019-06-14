46e84c3731e81803e969a0840e14c5e513a75943
Jordon Cotton <s188044@students.aie.edu>
2019-06-14 15:31:08 -0500

    save, load and inventory for cross platform

---
+++ Unity-CrossPlatformDevelopment/Assets/Jordon/InventoryBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryBehaviour : EventArgs
+{
+    private const int SLOTS = 9;
+
+    private List<InventoryBehaviour> mItems = new List<InventoryBehaviour>();
+
+    public event EventHandler<InventoryEventArgs> ItemAdded;
+
+    public void AddItem(IInventoryItem item)
+    {
+        if(mItems.Count < SLOTS)
+        {
+            Collider collider = (item as MonoBehaviour).GetComponent<Collider>();
+            if (collider.enabled)
+            {
+                collider.enabled = false;
+                mItems.Add(item);
+
+                item.Onpickup();
+
+                if (ItemAdded != null)
+                {
+                    ItemAdded(this, new InventoryEventArgs(item));
+                }
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+	
+}
