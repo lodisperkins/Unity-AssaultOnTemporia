d72213a0241a9a9f9ce2179a81f1e68a08ac9440
Jordon Cotton <s188044@students.aie.edu>
2019-06-20 17:56:59 -0500

    added code for save and load and a player positon to save and load in a level and that players health

---
+++ Unity-CrossPlatformDevelopment/Assets/Jordon/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level = 3;
+    public int health = 40;
+
+    public void SavePlayer()
+    {
+        Savebehaviour.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = Savebehaviour.LoadPlayer();
+        level = data.level;
+        health = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+
+    #region UI Methods
+
+    public void ChangeLevel(int amount)
+    {
+        level += amount;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        health += amount;
+    }
+    #endregion
+}
--- Unity-CrossPlatformDevelopment/Assets/Jordon/SaveBehaviour.cs
+++ Unity-CrossPlatformDevelopment/Assets/Jordon/SaveBehaviour.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
-public class Savebehaviour : MonoBehaviour {
+public static class Savebehaviour
+{
+    public static void SavePlayer (Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer ()
+    {
+        string path = Application.persistentDataPath + "/Player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save File not found in" + path);
+            return null;
+        }
+    }
+
 }
