30c06931beb4017a7a4eefb602f37d22384ff1b9
Lodis Perkins <s188043@students.aie.edu>
2019-06-26 12:37:43 -0500

    add physics events and listener behaviour

---
+++ Assets/Scripts/Matthew/PhysicsTriggerListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Matthew
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class PhysicsTriggerListener : MonoBehaviour
+    {
+        [Cinemachine.TagField]
+        public string CompareTagField;
+
+        public GameEvent TriggerEnterEvent;
+        public GameEvent TriggerExitEvent;
+        private void Start()
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+            TriggerEnterEvent = Resources.Load<GameEvent>("OnTriggerEnter");
+            TriggerExitEvent = Resources.Load<GameEvent>("OnTriggerExit");
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(CompareTagField))
+                TriggerEnterEvent.Raise(gameObject);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(CompareTagField))
+            {
+                TriggerExitEvent.Raise(gameObject);
+            }
+        }
+    }
+}
