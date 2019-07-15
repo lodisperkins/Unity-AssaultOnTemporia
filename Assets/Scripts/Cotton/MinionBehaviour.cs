3d62d60d42e67e9cb60f283c0218065ffae4545b
Jordon Cotton <s188044@students.aie.edu>
2019-07-15 15:19:01 -0500

    added MinionHealth script

--- Assets/Scripts/Cotton/MinionBehaviour.cs
+++ Assets/Scripts/Cotton/MinionBehaviour.cs
@@ -3,80 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 
     
+public class MinionBehaviour : MonoBehaviour
+{
+    public Wave[] Waves; // class to hold information per wave
+    public Transform[] SpawnPoints;
+    public float TimeBetweenEnemies = 2f;
 
-    public class MinionBehaviour : MonoBehaviour
+    private int _totalEnemiesInCurrentWave;
+    private int _enemiesInWaveLeft;
+    private int _spawnedEnemies;
+
+    private int _currentWave;
+    private int _totalWaves;
+
+    float speed = 200f;
+    float damage = 5f;
+
+    void Start()
     {
-        public Wave[] Waves; // class to hold information per wave
-        public Transform[] SpawnPoints;
-        public float TimeBetweenEnemies = 2f;
+        _currentWave = -1; // avoid off by 1
+        _totalWaves = Waves.Length - 1; // adjust, because we're using 0 index
 
-        private int _totalEnemiesInCurrentWave;
-        private int _enemiesInWaveLeft;
-        private int _spawnedEnemies;
+        StartNextWave();
+    }
 
-        private int _currentWave;
-        private int _totalWaves;
+    [System.Serializable]
+    public class Wave
+    {
+        public int EnemiesPerWave;
+        public GameObject Enemy;
+    }
 
-        void Start()
+    void StartNextWave()
+    {
+        _currentWave++;
+
+        // win
+        if (_currentWave > _totalWaves)
         {
-            _currentWave = -1; // avoid off by 1
-            _totalWaves = Waves.Length - 1; // adjust, because we're using 0 index
+            return;
+        }
 
+        _totalEnemiesInCurrentWave = Waves[_currentWave].EnemiesPerWave;
+        _enemiesInWaveLeft = 0;
+        _spawnedEnemies = 0;
+
+        StartCoroutine(SpawnEnemies());
+    }
+    
+    // Coroutine to spawn all of our enemies
+    IEnumerator SpawnEnemies()
+    {
+        GameObject enemy = Waves[_currentWave].Enemy;
+        while (_spawnedEnemies < _totalEnemiesInCurrentWave)
+        {
+            _spawnedEnemies++;
+            _enemiesInWaveLeft++;
+
+            int spawnPointIndex = Random.Range(0, SpawnPoints.Length);
+
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate(enemy, position: SpawnPoints[spawnPointIndex].position, rotation: SpawnPoints[spawnPointIndex].rotation);
+            yield return new WaitForSeconds(seconds: TimeBetweenEnemies);
+        }
+            yield return null;
+    }
+
+    // called by an enemy when they're defeated
+    public void EnemyDefeated()
+    {
+        _enemiesInWaveLeft--;
+
+        // We start the next wave once we have spawned and defeated them all
+        if (_enemiesInWaveLeft == 0 && _spawnedEnemies == _totalEnemiesInCurrentWave)
+        {
             StartNextWave();
         }
-
-        [System.Serializable]
-        public class Wave
-        {
-            public int EnemiesPerWave;
-            public GameObject Enemy;
-        }
-
-        void StartNextWave()
-        {
-            _currentWave++;
-
-            // win
-            if (_currentWave > _totalWaves)
-            {
-                return;
-            }
-
-            _totalEnemiesInCurrentWave = Waves[_currentWave].EnemiesPerWave;
-            _enemiesInWaveLeft = 0;
-            _spawnedEnemies = 0;
-
-            StartCoroutine(SpawnEnemies());
-        }
-    
-        // Coroutine to spawn all of our enemies
-        IEnumerator SpawnEnemies()
-        {
-            GameObject enemy = Waves[_currentWave].Enemy;
-            while (_spawnedEnemies < _totalEnemiesInCurrentWave)
-            {
-                _spawnedEnemies++;
-                _enemiesInWaveLeft++;
-
-                int spawnPointIndex = Random.Range(0, SpawnPoints.Length);
-
-                // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-                Instantiate(enemy, position: SpawnPoints[spawnPointIndex].position, rotation: SpawnPoints[spawnPointIndex].rotation);
-                yield return new WaitForSeconds(seconds: TimeBetweenEnemies);
-            }
-            yield return null;
-        }
-
-        // called by an enemy when they're defeated
-        public void EnemyDefeated()
-        {
-            _enemiesInWaveLeft--;
-
-            // We start the next wave once we have spawned and defeated them all
-            if (_enemiesInWaveLeft == 0 && _spawnedEnemies == _totalEnemiesInCurrentWave)
-            {
-                StartNextWave();
-            }
-        }
     }
+
+    public void Update()
+    {
+        transform.Rotate
+    }
+}
 
---
+++ Assets/Scripts/Cotton/MinionHealthBehaviour.cs
@@ -0,0 +1,93 @@
+﻿using Lodis;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cotton.Gameplay
+{
+    public class MinionHealthBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private IntVariable RespawnDelayRef;
+        private float RespawnDelay;
+        private float RespawnTime;
+        [SerializeField]
+        private IntVariable HealthRef;
+        [SerializeField]
+        private IntVariable BaseHealthRef;
+        private int health;
+        [SerializeField]
+        private Matthew.GameEvent OnMinionDeath;
+        [SerializeField]
+        private UnityEngine.Events.UnityEvent OnMinionRespawn;
+        private bool IsAlive;
+
+        void Start()
+        {
+            RespawnDelay = RespawnDelayRef.Val;
+            health = BaseHealthRef.Val;
+        }
+        public void Restart()
+        {
+            RespawnDelay = RespawnDelayRef.Val;
+            health = BaseHealthRef.Val;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Bullet"))
+            {
+                TakeDamage(other.gameObject);
+            }
+        }
+        private void TakeDamage(GameObject bullet)
+        {
+            if (CompareTag(bullet.GetComponent<BulletBehaviour>().shooter))
+            {
+                return;
+            }
+            else
+            {
+                health -= 1;
+                HealthRef.Val = health;
+                CheckHealth();
+            }
+        }
+        private void CheckHealth()
+        {
+            if (health <= 0)
+            {
+                OnMinionDeath.Raise(gameObject);
+                Die();
+            }
+        }
+        public void Die()
+        {
+            IsAlive = false;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            gameObject.GetComponent<CharacterController>().enabled = false;
+            RespawnTime = RespawnDelay + Time.time;
+        }
+        private void Respawn()
+        {
+            if (Time.time >= RespawnTime)
+            {
+                IsAlive = true;
+                health = BaseHealthRef.Val;
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
+                gameObject.GetComponent<BoxCollider>().enabled = true;
+                gameObject.GetComponent<CharacterController>().enabled = true;
+                OnMinionRespawn.Invoke();
+            }
+        }
+        void Update()
+        {
+            if (IsAlive == false)
+            {
+                Respawn();
+                return;
+            }
+        }
+    }
+}
+
