90cedca94894f6dcb7be030cd02274b06873b472
Jordon Cotton <s188044@students.aie.edu>
2019-07-19 10:12:45 -0500

    Modified minion spawn behaviour script

--- Assets/Scripts/Cotton/MinionSpawnBehaviour.cs
+++ Assets/Scripts/Cotton/MinionSpawnBehaviour.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class MinionSpawnBehaviour : MonoBehaviour
 {
+    MinionBehaviour Spawnpoint;
 	// Use this for initialization
 	void Start ()
-    { 
-		
+    {
+        Spawnpoint = GetComponent<MinionBehaviour>();
 	}
-	
+	public void EnableSpawn()
+    {
+        Spawnpoint.enabled = true;
+    }
+    public void DisableSpawn()
+    {
+        foreach(GameObject enemy in Spawnpoint.Enemies)
+        {
+            Destroy(enemy);
+        }
+        Spawnpoint.enabled = false;
+    }
 	// Update is called once per frame
 	void Update ()
     {
